6a0894f22c1cfd3627200dff410c3a45bab88f1b
emptycan1010 <emptycan1010@gmail.com>
2022-04-06 00:03:46 +0900

    Update

--- discordbot-golang.go
+++ discordbot-golang.go
@@ -5,8 +5,31 @@ import (
 	"fmt"
 	"github.com/andersfylling/disgord"
 	"github.com/andersfylling/disgord/std"
+	"github.com/sirupsen/logrus"
 	"os"
 )
+
+var log = logrus.New()
+
+var commands = []*disgord.CreateApplicationCommand{
+	{
+		Name:        "test_command",
+		Description: "just testing",
+		Options: []*disgord.ApplicationCommandOption{
+			{
+				Name:        "test_option",
+				Type:        disgord.OptionTypeString,
+				Description: "testing options",
+				Choices: []*disgord.ApplicationCommandOptionChoice{
+					{
+						Name:  "test_choice",
+						Value: "test_val",
+					},
+				},
+			},
+		},
+	},
+}
 
 func main() {
 	client := disgord.New(disgord.Config{
@@ -14,9 +37,20 @@ func main() {
 		Intents:  disgord.AllIntents(),
 	})
 	defer client.Gateway().StayConnectedUntilInterrupted()
+
+	u, err := client.BotAuthorizeURL(disgord.PermissionUseSlashCommands, []string{
+		"bot",
+		"applications.commands",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(u)
+
 	content, _ := std.NewMsgFilter(context.Background(), client)
 	content.SetPrefix("ping")
-
 	client.Gateway().
 		WithMiddleware(content.HasPrefix).
 		MessageCreate(func(s disgord.Session, evt *disgord.MessageCreate) {
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/andersfylling/disgord v0.35.1 // indirect
 	github.com/andersfylling/snowflake/v5 v5.0.1 // indirect
 	github.com/klauspost/compress v1.15.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	golang.org/x/crypto v0.0.0-20220315160706-3147a52a75dd // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
