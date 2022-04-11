e02b01515d7add3cc08db76e9111b0a39b0326e0
emptycan1010 <emptycan1010@gmail.com>
2022-04-11 22:37:12 +0900

    Command

--- discordbot-golang.go
+++ discordbot-golang.go
@@ -4,35 +4,36 @@ import (
 	"context"
 	"fmt"
 	"github.com/andersfylling/disgord"
-	"github.com/andersfylling/disgord/std"
+	"github.com/sirupsen/logrus"
 	"os"
 )
 
-//var log = logrus.New()
-//
-//var commands = []*disgord.CreateApplicationCommand{
-//	{
-//		Name:        "test_command",
-//		Description: "just testing",
-//		Options: []*disgord.ApplicationCommandOption{
-//			{
-//				Name:        "test_option",
-//				Type:        disgord.OptionTypeString,
-//				Description: "testing options",
-//				Choices: []*disgord.ApplicationCommandOptionChoice{
-//					{
-//						Name:  "test_choice",
-//						Value: "test_val",
-//					},
-//				},
-//			},
-//		},
-//	},
-//}
+var log = logrus.New()
+
+var commands = []*disgord.CreateApplicationCommand{
+	{
+		Name:        "test_command",
+		Description: "just testing",
+		Options: []*disgord.ApplicationCommandOption{
+			{
+				Name:        "test_option",
+				Type:        disgord.OptionTypeString,
+				Description: "testing options",
+				Choices: []*disgord.ApplicationCommandOptionChoice{
+					{
+						Name:  "test_choice",
+						Value: "test_val",
+					},
+				},
+			},
+		},
+	},
+}
 
 func main() {
 	client := disgord.New(disgord.Config{
 		BotToken: os.Getenv("DISCORD_TOKEN"),
+		Logger:   log,
 		Intents:  disgord.AllIntents(),
 	})
 	defer func(gateway disgord.GatewayQueryBuilder) {
@@ -53,11 +54,29 @@ func main() {
 
 	fmt.Println(u)
 
-	content, _ := std.NewMsgFilter(context.Background(), client)
-	content.SetPrefix("ping")
-	client.Gateway().
-		WithMiddleware(content.HasPrefix).
-		MessageCreate(func(s disgord.Session, evt *disgord.MessageCreate) {
-			_, _ = evt.Message.Reply(context.Background(), s, "pong")
+	client.Gateway().BotReady(func() {
+		for i := range commands {
+			// application command id is 0 here
+			// on a ready event, the client is updated to store the application id
+			// you can fetch the application id using the bot id (current user id) or copy it from
+			// the discord page.
+			if err = client.ApplicationCommand(0).Guild(923185824869285938).Create(commands[i]); err != nil {
+				log.Fatal(err)
+			}
+		}
+	})
+
+	client.Gateway().InteractionCreate(func(s disgord.Session, h *disgord.InteractionCreate) {
+		fmt.Printf("%+v", *h)
+		err := s.SendInteractionResponse(context.Background(), h, &disgord.CreateInteractionResponse{
+			Type: 4,
+			Data: &disgord.CreateInteractionResponseData{
+				Content:    "hello",
+				Components: []*disgord.MessageComponent{},
+			},
 		})
+		if err != nil {
+			log.Error(err)
+		}
+	})
 }
